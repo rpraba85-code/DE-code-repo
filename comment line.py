70ecb335d22d8a1103031aeeb4d92d9f97749249
Praba <rpraba85@gmail.com>
2026-09-16 17:40:00 +0530

    Add files via upload
    Python fundamentals

---
+++ comment line.py
@@ -0,0 +1,11 @@
+students = 100
+trainers = 2
+total = students + trainers
+print(total)
+#print("Welcome to Inceptez Python Learning")
+print("""This is Inceptez's "Python" class for Data Engineers & AI Engineers""")
+print("""
+    Welcome to Inceptez Technologies!
+    Python Training: Basics
+    Enjoy your learning journey.
+      """)
